bea6824c44d108eaf4de8338d0968529cc9f2627
Thapelo <thapeloletsapa@gmail.com>
2024-06-17 06:41:11 +0200

    "Added movie details (year and poster) to App component"

--- src/App.jsx
+++ src/App.jsx
@@ -44,11 +44,21 @@ useEffect(() =>{
         onClick={()=>{}}
         />
       </div>
+
       <div className='container'>
         <div className='movie'>
+
 <div>
   <p>{movie1.Year}</p>
 </div>
+
+<div>
+<img
+src={movie1.Poster} 
+alt={movie1.Title}
+/>
+</div>
+
         </div>
       </div>
     </div>
