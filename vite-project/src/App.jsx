1db7ca4ea3c48b9f088b38ddc2d11ca68de97827
Thapelo <thapeloletsapa@gmail.com>
2024-06-17 05:17:09 +0200

    "Added API URL and searchMovies function, and useEffect hook to App component"

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,25 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import { useEffect } from 'react';
 import './App.css'
 
-//5ea9ca27
 
-function App() {
+//5ea9ca27
+const API_URL = "http://www.omdbapi.com?apikey=5ea9ca27";
+
+const App = () => {
+  
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s={title}`);
+    const data = await response.json();
+
+    console.log(data)
+  }
+
+useEffect(() =>{
+   
+}, []);
+
   return (
     <h1>App</h1>
   )
