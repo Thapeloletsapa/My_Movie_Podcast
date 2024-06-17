5d99d932ea54903a9cb8b165f2a8f96ff949b308
Thapelo <thapeloletsapa@gmail.com>
2024-06-17 09:00:26 +0200

    "Refactored App component to add search functionality and updated MovieCard component to destructure props"

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,13 @@ import MovieCard from "./MovieCard";
 //5ea9ca27
 const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=5ea9ca27";
 
-const movie1 = {
-  Title: "Spider-Man Title Reveal",
-  Year: "2021",
-  imdbID: "tt14122734",
-  Type: "movie",
-  Poster:
-    "https://m.media-amazon.com/images/M/MV5BNjRjMmQ2NDQtNmI5NC00N2EwLTkwYWQtOTM2OGZjMmI5YmRjXkEyXkFqcGdeQXVyMTI0NTA1MDI3._V1_SX300.jpg",
-};
-
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
@@ -26,21 +22,21 @@ const App = () => {
     setMovies(data.Search);
   };
 
-  useEffect(() => {
-    searchMovies("Hulk");
-  }, []);
-
   return (
     <div className="app">
       <h1>Bioskop</h1>
 
       <div className="search">
         <input
-          value="Superman"
+          value={searchTerm}
           placeholder="Search for a movie..."
-          onChange={() => {}}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <img src={SearchIcon} alt="search icon" onClick={() => {}} />
+        <img
+          src={SearchIcon}
+          alt="search icon"
+          onClick={() => searchMovies(searchTerm)}
+        />
       </div>
 
       {movies?.length > 0 ? (
