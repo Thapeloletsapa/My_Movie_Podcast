d5f9aa67882f9733e8fab61dbf6d354ca58e26af
Thapelo <thapeloletsapa@gmail.com>
2024-06-20 12:02:56 +0200

    "Added react-redux dependency and updated package-lock.json"

---
+++ src/Components/UI/Button.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { styled } from 'styled-components';
+
+const Button = styled.button`
+  padding: 0.5rem 0.7rem;
+  border: 1px solid black;
+  border-radius: 2rem;
+  background-color: ${(props) => props.$backgroundColor};
+`;
+
+const Buttons = (props) => {
+  return (
+    <Button $backgroundColor={props.$backgroundColor}>{props.children}</Button>
+  );
+};
+
+export default Buttons;
