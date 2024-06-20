3b6d5d8bd0d253369d20ddf248f2380621c63caf
Thapelo <thapeloletsapa@gmail.com>
2024-06-20 10:23:18 +0200

    "Refactor Episode component to use Redux, Supabase, and styled components; add favourite functionality and redesign UI"

--- src/Components/Episode.jsx
+++ src/Components/Episode.jsx
@@ -1,17 +1,98 @@
-/* eslint-disable no-undef */
-/* eslint-disable react/prop-types */
-/* eslint-disable no-unused-vars */
-import React, { useState } from "react";
-import AudioPlayer from "./AudioPlayer";
+import React, { useState, useEffect } from "react";
+import { setFavouriteSwitch } from "../globalState/reducers/podcastsReducer";
+import { useSelector, useDispatch } from "react-redux";
+import supabase from "../supabase/client";
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
+import { styled } from "styled-components";
+const EpisodeContainer = styled.div`
+  background-color: #242424;
+  margin: 1rem 0;
+  padding: 0.8rem;
+  border-radius: 0.25rem;
+`;
+const ImageDescriptionContainer = styled.div`
+  display: flex;
+  gap: 1rem;
+  margin: 1rem 0;
+`;
+const Description = styled.p`
+  font-size: 0.8rem;
+  margin: 0.7rem 0;
+`;
+const Image = styled.img`
+  width: 40%;
+`;
+const Episode = ({
+  item,
+  id,
+  showId,
+  currentSeason,
+  image,
+  showTitle,
+  updated,
+}) => {
+  const [isFavourite, setIsFavourite] = useState(false);
+  const { favourites } = useSelector((state) => state.podcastsReducer);
+  const { description, episode, file, title } = item;
+  const dispatch = useDispatch();
+  const episodeWithId = {
+    ...item,
+    id,
+    showId,
+    currentSeason,
+    image,
+    showTitle,
+    updated,
+  };
 
-function Episode({ episode }) {
+  const addToFavorites = async (ep) => {
+    if (favourites !== null) {
+      for (const fav of favourites) {
+        if (fav.episodeId === id) return;
+      }
+    }
+    const { data, error } = await supabase
+      .from("userFavourites")
+      .insert([
+        { episodeId: id, episodeDetails: JSON.stringify(ep), showId: showId },
+      ])
+      .select();
+    // dispatch(setFavourites({ name: showId, value: ep }));
+    setIsFavourite(!isFavourite);
+    dispatch(setFavouriteSwitch());
+  };
+
+  const removeFromFavourites = async () => {
+    const { error } = await supabase
+      .from("userFavourites")
+      .delete()
+      .eq("episodeId", id);
+    setIsFavourite(!isFavourite);
+    dispatch(setFavouriteSwitch());
+  };
   return (
-    <div>
-      <h2>{episode.title}</h2>
-      <p>File: {episode.file}</p>
-      <button onClick={() => handleFavourite(episode.id)}>Favourite</button>
-    </div>
+    <EpisodeContainer>
+      <h4>{`${episode}: ${title}`}</h4>
+      <ImageDescriptionContainer>
+        <Image src={image} alt={title} />
+        <div>
+          <Description>{description}</Description>
+          {isFavourite ? (
+            <button onClick={removeFromFavourites}>
+              Remove from favourites <AiFillHeart />
+            </button>
+          ) : (
+            <button onClick={() => addToFavorites(episodeWithId)}>
+              Add to favourites <AiOutlineHeart />
+            </button>
+          )}
+        </div>
+      </ImageDescriptionContainer>
+      <audio controls>
+        <source src={file} type="audio/mp3" />
+      </audio>
+    </EpisodeContainer>
   );
-}
+};
 
 export default Episode;
