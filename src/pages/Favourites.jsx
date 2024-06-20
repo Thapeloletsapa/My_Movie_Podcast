70a32d3c6ca8fd06b258c25e753a66a0d96dcb67
Thapelo <thapeloletsapa@gmail.com>
2024-06-20 09:47:25 +0200

    "Added styled components and Favourite component to Favourites.jsx"

--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -8,3 +8,37 @@ import {
   setFavoritesSorting,
   setFavouritesDisplayedPodcasts,
 } from '../globalState/reducers/podcastsReducer';
+
+
+const FavouritesContainer = styled.div`
+  color: white;
+`;
+const EpisodeContainer = styled.div`
+  background-color: #242424;
+  margin: 1rem 0.5rem;
+  padding: 0.8rem;
+  border-radius: 0.25rem;
+`;
+const ImageDescriptionContainer = styled.div`
+  display: flex;
+  gap: 1rem;
+  margin: 1rem 0;
+`;
+const Description = styled.p`
+  font-size: 0.8rem;
+  margin: 0.7rem 0;
+`;
+const Image = styled.img`
+  width: 40%;
+`;
+const Favourite = ({ details, showNameId }) => {
+  const {
+    title,
+    episode,
+    file,
+    image,
+    description,
+    currentSeason,
+    showTitle,
+    showId,
+  } = details;
