dde2feb0f155e5291e1c9f5385f15bbaace97837
Thapelo <thapeloletsapa@gmail.com>
2024-06-20 09:44:02 +0200

    "Updated package-lock.json and package.json with new dependencies, and modified Home.jsx to import and use styled components, react-redux, and other components."

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,53 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-export default function Home() {
+import { styled } from "styled-components";
+import { HomePodcast, Navbar } from "../components";
+import { setHomePageDisplayedPodcasts } from "../globalState/reducers/podcastsReducer";
+
+const Main = styled.main`
+  padding: 0.25rem 1rem;
+`;
+
+const PodcastsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 2rem;
+`;
+
+const Home = () => {
+  const {
+    allPodcasts,
+    searchInput,
+    homePageDisplayedPodcasts,
+    isLoading,
+    sorting,
+    isLoggedIn,
+  } = useSelector((state) => state.podcastsReducer);
   return (
-    <div>
-      <h1>HOME</h1>
-    </div>
+    <>
+      <Navbar
+        podcastArray={homePageDisplayedPodcasts}
+        defaultArray={allPodcasts}
+        setDisplayAction={setHomePageDisplayedPodcasts}
+      />
+      <Main>
+        {isLoading ? (
+          <h1>Loading...</h1>
+        ) : (
+          <PodcastsContainer>
+            {homePageDisplayedPodcasts.map((item, index) => {
+              const { description, image, id, title, genres } = item;
+
+              return <HomePodcast key={id} item={item} />;
+            })}
+          </PodcastsContainer>
+        )}
+      </Main>
+    </>
   );
-}
+};
+
+export default Home;
