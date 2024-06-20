d63134b6a0e7eb56c4b0d41a7b8bb671c60b6773
Thapelo <thapeloletsapa@gmail.com>
2024-06-20 10:30:47 +0200

    "Added 5 component exports to index.jsx"

---
+++ src/Components/index.jsx
@@ -0,0 +1,6 @@
+import HomePodcast from './HomePodcast';
+import Seasons from './Seasons';
+import Episode from './Episode';
+import AllEpisodesPerSeason from './AllEpisodesPerSeason';
+import Navbar from './Navbar';
+export { Navbar, HomePodcast, Seasons, Episode, AllEpisodesPerSeason };
