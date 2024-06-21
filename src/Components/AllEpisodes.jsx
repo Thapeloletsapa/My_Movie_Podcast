407c358151236690427587d1ef61794634454b57
Thapelo <thapeloletsapa@gmail.com>
2024-06-21 14:25:25 +0200

    "Added JSDoc comments and reformatted code in AllEpisodes component"

--- src/Components/AllEpisodes.jsx
+++ src/Components/AllEpisodes.jsx
@@ -1,34 +1,51 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React from "react";
-import Episode from "./Episode";
-import { useSelector } from "react-redux";
+import React from 'react';
+import Episode from './Episode';
+import { useSelector } from 'react-redux';
 
-// eslint-disable-next-line react-refresh/only-export-components
+/**
+ * AllEpisodes component
+ * 
+ * This component renders a list of episodes for the current season of a podcast.
+ * It uses the `useSelector` hook from `react-redux` to retrieve the current podcast and season from the store.
+ * 
+ * @param {object} props - Component props
+ * @param {array} props.currentSeasonEpisodes - Array of episode objects for the current season
+ */
 const AllEpisodes = ({ currentSeasonEpisodes }) => {
-  const { currentPodcast, currentSeason } = useSelector(
-    (state) => state.podcastsReducer
-  );
+  /**
+   * Retrieve the current podcast and season from the store using `useSelector`
+   */
+  const { currentPodcast, currentSeason } = useSelector((state) => state.podcastsReducer);
 
   return (
     <div>
       <div>
-        {currentSeasonEpisodes.map((item) => {
-          const id = `Show:${currentPodcast.id} Season:${currentSeason} Episode:${item.episode}`;
+        {
+          /**
+           * Map over the current season episodes and render an Episode component for each one
+           */
+          currentSeasonEpisodes.map((item) => {
+            const id = `Show:${currentPodcast.id} Season:${currentSeason} Episode:${item.episode}`;
 
-          return (
-            <Episode
-              showTitle={currentPodcast.title}
-              image={currentPodcast.image}
-              id={id}
-              key={item.episode}
-              item={item}
-              showId={currentPodcast.id}
-              currentSeason={currentSeason}
-              updated={currentPodcast.updated}
-            />
-          );
-        })}
+            return (
+              <Episode
+                /**
+                 * Pass props to the Episode component
+                 */
+                showTitle={currentPodcast.title}
+                image={currentPodcast.image}
+                id={id}
+                key={item.episode}
+                item={item}
+                showId={currentPodcast.id}
+                currentSeason={currentSeason}
+                updated={currentPodcast.updated}
+              />
+            );
+          })
+        }
       </div>
     </div>
   );
